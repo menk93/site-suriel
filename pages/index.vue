c6860fb39f61722dd206410524afcd2594d06815
Matheus Henry <menk.henry93@gmail.com>
2020-03-02 00:03:33 -0300

    Background

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,22 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-   
-  </v-layout>
+  <v-img id="imgFundo" :src="require('@/assets/bg-white.jpg')">
+    <v-layout row wrap justify-center>
+      <v-flex class="imgLogo pt-8">
+        <v-img :src="require('@/assets/logo.png')"></v-img>
+      </v-flex>
+    </v-layout>
+  </v-img>
+  
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
-export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  }
-}
+export default {};
 </script>
+
+<style lang="scss">
+.imgLogo{
+  max-width: 50%;
+  max-height: 30%;
+
+}
+</style>
